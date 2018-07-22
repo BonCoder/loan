16a6fa05ed70505100209b45f565c11b8a718cf6
BobCoder <bob@bobcoder.cc>
2018-07-22 14:43:40 +0800

    chore: 修改配置文件

--- application/extra/qiniu.php
+++ application/extra/qiniu.php
@@ -4,5 +4,5 @@ return [
         'AccessKey' => 'Ip4Ai9cExoiOdt-vLsfMlTgo0rHyJGjzvtSsW0DS',//你的accessKey
         'SecretKey' => 'Ik8WWy3KPG69ATraq1IHacVAg2reScITRUUVzF8j',//你的secretKey
         'Bucket' => 'loan',//上传的空间
-        'Domain'=>'file.bobcoder.cc',//空间绑定的域名
+        'Domain'=>'http://file.bobcoder.cc',//空间绑定的域名
     ];
